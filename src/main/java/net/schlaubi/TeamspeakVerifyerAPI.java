288a747fcbcf16475560c44ac723e5fdb60166a5
Michael R.|SchlaumeierTV <michael@rittmeister.in>
2017-10-12 12:33:00 +0200

    Api Update - Fixed API

--- src/main/java/net/schlaubi/TeamspeakVerifyerAPI.java
+++ src/main/java/net/schlaubi/TeamspeakVerifyerAPI.java
@@ -1,17 +1,18 @@
-package net.schlaubi.bungee.teamspeakverifyer;
+package net.schlaubi;
 
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.schlaubi.util.MySQL;
+import org.bukkit.entity.Player;
 
-public class TeamSpeakVerifyerAPI {
+public class TeamspeakVerifyerAPI {
 
-    public static boolean isVerified(ProxiedPlayer player){
+
+    public static boolean isVerified(Player player){
         if(MySQL.userExists(player)){
             return true;
         }
         return false;
     }
-
     public static boolean isVerified(String identity){
         if(MySQL.userExists(identity)){
             return true;
@@ -23,7 +24,18 @@ public class TeamSpeakVerifyerAPI {
         return MySQL.getValue(id, "uuid");
     }
 
+    public static String getDatabaseId(Player player){
+        return MySQL.getValue(player, "uuid");
+    }
+
     public static String getDatabaseId(ProxiedPlayer player){
         return MySQL.getValue(player, "uuid");
+    }
+
+    public static boolean isVerified(ProxiedPlayer player){
+        if(MySQL.userExists(player)){
+            return true;
+        }
+        return false;
     }
 }
